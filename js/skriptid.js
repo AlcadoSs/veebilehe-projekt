6053ec894a597e4bd5cbdfb4b02f1cb92407f15c
rteessar <rteessar@gmail.com>
2018-11-29 19:21:56 +0200

    Veebileht peaks pmst valmis olema

--- js/skriptid.js
+++ js/skriptid.js
@@ -6,3 +6,17 @@ function myFunction() {
         x.style.display = "none";
     }
 }
+
+function iCanHasBeer() {
+    var time = new Date();
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    var output = document.getElementById('sisu');
+    var remaining;
+    if (hours > 10 || hours < 22) {
+        remaining = (22 * 60) - (hours * 60) - minutes;
+        output.innerHTML = '<p>you can has beer!</p><p>Time till no beer: ' + (remaining - (remaining % 60)) / 60 + ' : ' + (remaining % 60) + '</p>';
+    } else {
+        output.innerHTML = '<p>you can no has beer...</p>';
+    }
+}
